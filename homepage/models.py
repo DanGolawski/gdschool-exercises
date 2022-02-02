8570f401d6a2fbd376f994dfdd2dbf34746d7f00
DanGolawski <dangolawski@gmail.com>
2022-02-02 13:20:06 +0100

    change model

--- homepage/models.py
+++ homepage/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Exercise(models.Model):
-    id = models.CharField(max_length=5, primary_key=True)
     book = models.CharField(max_length=15)
     chapter = models.IntegerField()
     exercise = models.IntegerField()
